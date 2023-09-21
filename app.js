c0981e0813fe0f4a13d982f77a145bb7b811ade8
Tanisha Tomar <tanisha@Tanishas-MacBook-Air.local>
2023-09-21 21:44:44 +0530

    update

---
+++ app.js
@@ -0,0 +1,21 @@
+
+function goToPage(ref){
+window.location.href = ref;
+}
+
+function isMobile() {
+    const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+    return regex.test(navigator.userAgent);
+  }
+  
+//   if (isMobile()) {
+//     console.log("Mobile device detected");
+//   } else {
+//     console.log("Desktop device detected");
+// }
+
+// if(!isMobile()){
+//     document.body.style.backgroundImage = "url('web2.jpg')";
+// }else{
+//     document.body.style.backgroundImage = "url('web3.jpeg')";
+// }
